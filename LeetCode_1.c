4a2461494e763408f22c484bb9b04ce031612f8c
keer123456789 <15110074528@163.com>
2019-12-26 18:09:26 +0800

    1

---
+++ LeetCode_1.c
@@ -0,0 +1,43 @@
+//
+// Created by keer on 2019/12/26.
+//
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ *
+ *
+ * 实例：
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+#include <stdio.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i=0,j=0;
+    int a=0,b=0;
+    int *num=nums;
+    for(i=0;i<numsSize-1;i++){
+        a = *num;
+        nums=num;
+        nums++;
+        for(j=i+1;j<numsSize;j++) {
+            b = *nums;
+            if (a + b == target) {
+                *returnSize = i;
+                returnSize ++;
+                *returnSize = j;
+            }
+            nums++;
+        }
+        num++;
+    }
+    return returnSize;
+}
+void main(){
+    int nums[]={2,11,15,7};
+    int sum1[]={0,0};
+    int *sum=sum1;
+    sum=twoSum(nums,4,9,sum);
+    printf("%d,,,%d",sum1[0],sum1[1]);
+}
---
+++ cmake-build-debug/CMakeFiles/LeetCode_1.c.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeetCode_1.c.dir/LeetCode_1.c.obj"
+  "LeetCode_1.c.pdb"
+  "LeetCode_1.c.exe"
+  "LeetCode_1.c.exe.manifest"
+  "libLeetCode_1.c.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LeetCode_1.c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
